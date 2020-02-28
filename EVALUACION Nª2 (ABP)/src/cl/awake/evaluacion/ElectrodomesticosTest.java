30017db369a94cd6e7f220579dcb321b01d2cc75
Matias Eduardo Flores Farias <31407903+Mathias182@users.noreply.github.com>
2020-02-28 15:53:33 -0300

    cambios realizados
    Cambios realizados
    agregar valores

--- EVALUACION Nª2 (ABP)/src/cl/awake/evaluacion/ElectrodomesticosTest.java
+++ EVALUACION Nª2 (ABP)/src/cl/awake/evaluacion/ElectrodomesticosTest.java
@@ -1,64 +1,7 @@
 package cl.awake.evaluacion;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ElectrodomesticosTest {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void testElectrodomesticos() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testElectrodomesticosDoubleDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testElectrodomesticosStringCharDoubleDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetPrecio_base() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetColor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetConsumo_energetico() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetPeso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testComprobarConsumoEnergetico() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testComprobarColor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testPrecioFinal() {
-		fail("Not yet implemented");
-	}
 
 }
--- EVALUACION Nª2 (ABP)/src/cl/awake/evaluacion/Main.java
+++ EVALUACION Nª2 (ABP)/src/cl/awake/evaluacion/Main.java
@@ -19,27 +19,28 @@ public class Main {
 	        
 	        Electrodomesticos[] arrayElectro = new Electrodomesticos[10];
 	        
-	        arrayElectro[0] = new Electrodomesticos(180300, 55);
-	        arrayElectro[1] = new Lavadora("ROJO", 450, 200321, 'A', 20);
+	        arrayElectro[0] = new Lavadora(300, 200);
+	        arrayElectro[1] = new Lavadora("ROJO", 450, 200, 'A', 20);
 	        arrayElectro[2] = new Electrodomesticos();
-	        arrayElectro[3] = new Lavadora(189290 ,400);
-	        arrayElectro[4] = new Televicion(123234, 23);
-		arrayElectro[5] = new Lavadora(150900, 40);
-		arrayElectro[6] = new Lavadora(999000, 90);
-	        arrayElectro[7] = new Televicion(80300, 25);
-	        arrayElectro[8] = new Electrodomesticos(502500, 50);
-	        arrayElectro[9] = new Televicion(325000, 12);
+	        arrayElectro[3] = new Lavadora(189 ,400);
+	        arrayElectro[4] = new Televicion(123.9, 23);
+	        arrayElectro[5] = new Lavadora(150.900, 40);
+	        arrayElectro[6] = new Lavadora(999.0, 90);
+	        arrayElectro[7] = new Televicion(80.9, 25);
+	        arrayElectro[8] = new Electrodomesticos(50.9, 50);
+	        arrayElectro[9] = new Televicion(32.9, 12);
 	       
 	        
 	        return arrayElectro;
 	    }
 	    public static void mostrarPreciosFinales(Electrodomesticos[] arrayElectro) {
-	        
 	    	/**
 			 * Variables de entradas 
 			 * @param valorElectro, totalElectrodomesticos, totalTv, totalLavadoras.
 			 */
 			double valorElectro=0;
+			double valorElectroTV=0;
+			double valorElectroLavadora=0;
 	        int totalElectrodomesticos = 0;
 	        int totalTV = 0;
 	        int totalLavadoras = 0;
@@ -49,16 +50,18 @@ public class Main {
 				
 
 				if (arrayElectro[i] instanceof Electrodomesticos) {					
-					valorElectro += arrayElectro[i].precioFinal();
+					valorElectro  += arrayElectro[i].precioFinal();
 					totalElectrodomesticos++;
 				}
 				if (arrayElectro[i] instanceof Lavadora) {
 					System.out.println("El Valor de la lavadora es:  " + arrayElectro[i].precioFinal());				
 					totalLavadoras++;
+					valorElectroLavadora += arrayElectro[i].precioFinal();
 				}
 				if (arrayElectro[i] instanceof Televicion ) {
 					System.out.println("El valor de la televicion es: " + arrayElectro[i].precioFinal());
 					totalTV++;
+					valorElectroTV += arrayElectro[i].precioFinal();
 				}
 				
 			}
@@ -72,6 +75,8 @@ public class Main {
 			System.out.println("Cantidad no definida  :" + (totalTV+totalLavadoras-totalElectrodomesticos));
 			System.out.println("\t  \n Valores Totales \n ");
 			System.out.println("Electrodomesticos son: " + valorElectro);
+			System.out.println("Lavadoras son: "+ valorElectroLavadora);
+			System.out.println("Televisores son: "+ valorElectroTV);
 		}
 	    
 }
